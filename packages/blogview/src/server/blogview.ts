8d6d1e823d94261bf94e89d192f68b92f23d82c7
mkizka <g.mkizka@gmail.com>
2022-01-07 20:34:00 +0900

    更新

--- packages/blogview/src/server/blogview.ts
+++ packages/blogview/src/server/blogview.ts
@@ -1,2 +1,33 @@
-// @ts-ignore
-console.log(1);
+import path from "path";
+import { createServer } from "http";
+
+import express from "express";
+import history from "connect-history-api-fallback";
+
+export function getEntry(req: express.Request, res: express.Response) {
+  res.json({ article: null });
+}
+
+export function getEntries(req: express.Request, res: express.Response) {
+  res.json({ articles: null });
+}
+
+const main = async () => {
+  const app = express();
+  app.get(`/api/entry`, getEntries);
+  app.get(`/api/entry/:slug`, getEntry);
+
+  // serve static files built by vite
+  app.use(history());
+  app.use(
+    express.static(path.join(__dirname, "../../client"), {
+      setHeaders: (res) => {
+        res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+      },
+    })
+  );
+
+  createServer(app).listen(8000);
+};
+
+main();
