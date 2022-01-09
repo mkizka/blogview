33ddda27923186e06b51421a65028401701c8851
mkizka <g.mkizka@gmail.com>
2022-01-09 23:57:03 +0900

    temp

--- packages/blogview-utils/index.ts
+++ packages/blogview-utils/index.ts
@@ -1,21 +1,10 @@
-import { unified } from "unified";
-import rehypeStringify from "rehype-stringify";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkParse from "remark-parse";
-import remarkStringify from "remark-stringify";
-import remarkRehype from "remark-rehype";
-import * as remarkExtract from "remark-extract-frontmatter";
-import { rehypeHatenaEmbed } from "@mkizka/rehype-hatena-embed";
-import { parse as yaml } from "yaml";
+import * as markdownIt from "markdown-it";
+import * as remark from "remark";
+import * as remarkFrontmatter from "remark-frontmatter";
+import * as remarkParseFrontmatter from "remark-parse-frontmatter";
 
 export async function md2html(md: string) {
-  const processor = unified()
-    .use(remarkParse)
-    .use(remarkFrontmatter)
-    .use(remarkRehype)
-    .use(rehypeHatenaEmbed)
-    .use(rehypeStringify);
-  return (await processor.process(md)).value as string;
+  return markdownIt().render(md);
 }
 
 export type BlogMeta = {
@@ -27,10 +16,8 @@ export type BlogMeta = {
 };
 
 export async function md2meta(md: string) {
-  const processor = unified()
-    .use(remarkParse)
-    .use(remarkStringify)
-    .use(remarkFrontmatter)
-    .use(remarkExtract, { yaml });
-  return (await processor.process(md)).data as BlogMeta;
+  // @ts-ignore
+  const processor = remark().use(remarkFrontmatter).use(remarkParseFrontmatter);
+  // @ts-ignore
+  return (await processor.process(md)).data.frontmatter as BlogMeta;
 }
