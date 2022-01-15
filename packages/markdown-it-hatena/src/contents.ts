9e2b2ebc05214329babe92f34d44be504a3a04dc
mkizka <g.mkizka@gmail.com>
2022-01-15 20:22:24 +0900

    h1からh6までが見出しになるように

--- packages/markdown-it-hatena/src/contents.ts
+++ packages/markdown-it-hatena/src/contents.ts
@@ -4,5 +4,6 @@ export const contentsPlugin: MarkdownIt.PluginSimple = (md) => {
   md.use(require("markdown-it-anchor"));
   md.use(require("markdown-it-table-of-contents"), {
     markerPattern: /^\[:contents\]/i,
+    includeLevel: [1, 2, 3, 4, 5, 6],
   });
 };
