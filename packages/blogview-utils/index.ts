306976e6cd5a7b1b03c25a4dfe64d6dc5c7b1e4a
mkizka <g.mkizka@gmail.com>
2022-01-17 00:34:51 +0900

    html: true

--- packages/blogview-utils/index.ts
+++ packages/blogview-utils/index.ts
@@ -5,7 +5,9 @@ import remarkParseFrontmatter from "remark-parse-frontmatter";
 import markdownItHatena from "@mkizka/markdown-it-hatena";
 
 export async function md2html(md: string) {
-  return markdownIt({ linkify: true }).use(markdownItHatena).render(md);
+  return markdownIt({ linkify: true, html: true })
+    .use(markdownItHatena)
+    .render(md);
 }
 
 export type BlogMeta = {
