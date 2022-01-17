cbfa24ce1296da36ff4464c6dcb8d015b6f2d474
mkizka <g.mkizka@gmail.com>
2022-01-17 18:50:08 +0900

    型修正

--- packages/blogview/src/server/utils/transformers.ts
+++ packages/blogview/src/server/utils/transformers.ts
@@ -4,6 +4,8 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkParseFrontmatter from "remark-parse-frontmatter";
 import markdownItHatena from "@mkizka/markdown-it-hatena";
 
+import { BlogMeta } from "../../common/types";
+
 export async function md2html(md: string) {
   return markdownIt({ linkify: true, html: true })
     .use(markdownItHatena)
@@ -11,8 +13,7 @@ export async function md2html(md: string) {
 }
 
 export async function md2meta(md: string) {
-  // @ts-ignore
   const processor = remark().use(remarkFrontmatter).use(remarkParseFrontmatter);
-  // @ts-ignore
-  return (await processor.process(md)).data.frontmatter as BlogMeta;
+  const data = (await processor.process(md)).data as { frontmatter: BlogMeta };
+  return data.frontmatter;
 }
