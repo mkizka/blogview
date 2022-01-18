34d6e0b78810b157ecbf2da02d3456d9811387e7
mkizka <g.mkizka@gmail.com>
2022-01-18 13:20:15 +0900

    ツイート埋め込みに対応

---
+++ packages/blogview-vscode/tweet.js
@@ -0,0 +1,28 @@
+// https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
+window.twttr = (function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function (f) {
+    t._e.push(f);
+  };
+
+  return t;
+})(document, "script", "twitter-wjs");
+
+function loadTweet() {
+  window.twttr.widgets.load(
+    document.getElementsByClassName("twitter-tweet-container")
+  );
+}
+
+window.addEventListener("vscode.markdown.updateContent", loadTweet);
+
+loadTweet();
