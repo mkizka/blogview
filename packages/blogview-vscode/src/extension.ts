d8a05bdc6efa8ab883faf6b726a1e8cb7ac66460
mkizka <g.mkizka@gmail.com>
2022-01-23 00:43:42 +0900

    VSCodeの設定にオプション追加

--- packages/blogview-vscode/src/extension.ts
+++ packages/blogview-vscode/src/extension.ts
@@ -1,9 +1,15 @@
-import type * as MarkdownIt from "markdown-it";
+import vscode from "vscode";
+import type MarkdownIt from "markdown-it";
+import markdownItHatena from "markdown-it-hatena";
 
 export function activate() {
+  const config = vscode.workspace.getConfiguration("blogview");
   return {
     extendMarkdownIt(md: MarkdownIt) {
-      return md.use(require("markdown-it-hatena"));
+      return md.use(markdownItHatena, {
+        twitter: config.get("enableTwitter"),
+        youtube: config.get("enableYoutube"),
+      });
     },
   };
 }
