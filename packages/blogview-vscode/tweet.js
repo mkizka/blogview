e75abf3b55ec3708b07498a8871d61ae2f1ecba0
mkizka <g.mkizka@gmail.com>
2022-01-22 14:06:35 +0900

    ツイート埋め込みがある場合のみスクリプトを読み込むように

--- packages/blogview-vscode/tweet.js
+++ packages/blogview-vscode/tweet.js
@@ -1,24 +1,35 @@
-// https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
-window.twttr = (function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
+function loadScript() {
+  // https://developer.twitter.com/en/docs/twitter-for-websites/javascript-api/guides/set-up-twitter-for-websites
+  window.twttr = (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
 
-  t._e = [];
-  t.ready = function (f) {
-    t._e.push(f);
-  };
+    t._e = [];
+    t.ready = function (f) {
+      t._e.push(f);
+    };
 
-  return t;
-})(document, "script", "twitter-wjs");
+    return t;
+  })(document, "script", "twitter-wjs");
+}
+
+function shouldLoadScript() {
+  return document.getElementsByClassName("twitter-tweet").length > 0;
+}
 
 function loadTweet() {
-  window.twttr.widgets.load();
+  if (shouldLoadScript()) {
+    loadScript();
+  }
+  if ("twttr" in window) {
+    window.twttr.widgets.load();
+  }
 }
 
 window.addEventListener("vscode.markdown.updateContent", loadTweet);
