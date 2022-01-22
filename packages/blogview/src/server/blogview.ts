470e40f0df93c0ef35e9b2ef1e208985e351f7a0
mkizka <g.mkizka@gmail.com>
2022-01-22 18:07:16 +0900

    cac導入

--- packages/blogview/src/server/blogview.ts
+++ packages/blogview/src/server/blogview.ts
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
+import cac from "cac";
+
 import { startLocalChangesWatcher, startServer } from "./utils/server.js";
 import { port } from "../common/config.js";
 import { createApp } from "./api/app.js";
 
-const main = async () => {
-  const app = createApp();
-  const server = await startServer(app, port);
-  await startLocalChangesWatcher(server, `${process.cwd()}/entry/*.md`);
-};
+const cli = cac();
 
-main();
+cli //
+  .command("")
+  .action(async () => {
+    const app = createApp();
+    const server = await startServer(app, port);
+    await startLocalChangesWatcher(server, `${process.cwd()}/entry/*.md`);
+  });
+
+cli.help();
+
+cli.parse();
