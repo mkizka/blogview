d5a2492bf198b11cb9a85b22416d22d39d2e30b0
mkizka <g.mkizka@gmail.com>
2022-01-27 13:39:30 +0900

    修正

--- packages/blogview-vscode/src/extension.ts
+++ packages/blogview-vscode/src/extension.ts
@@ -5,10 +5,8 @@ import { markdownItHatena } from "markdown-it-hatena";
 export function activate() {
   const config = vscode.workspace.getConfiguration("blogview");
   const options = {
-    link: {
-      twitter: config.get("enableTwitter"),
-      youtube: config.get("enableYoutube"),
-    },
+    twitter: config.get("enableTwitter"),
+    youtube: config.get("enableYoutube"),
   };
   return {
     extendMarkdownIt(md: MarkdownIt) {
