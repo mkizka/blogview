67c61091bf42963f23f16da4941df72cbc3e71de
mkizka <g.mkizka@gmail.com>
2022-05-24 22:38:20 +0900

    基礎作成

--- packages/blogview-vscode/src/extension.ts
+++ packages/blogview-vscode/src/extension.ts
@@ -2,6 +2,8 @@ import vscode from "vscode";
 import type MarkdownIt from "markdown-it";
 import { HatenaPluginOptions, markdownItHatena } from "markdown-it-hatena";
 
+import { TreeDataProvider } from "./treeView/provider";
+
 export function getOptions(): HatenaPluginOptions {
   const config = vscode.workspace.getConfiguration("blogview");
   return {
@@ -10,7 +12,18 @@ export function getOptions(): HatenaPluginOptions {
   };
 }
 
+const provider = new TreeDataProvider();
+
 export function activate() {
+  console.log("blogview activated");
+  vscode.window.registerTreeDataProvider("blogview", provider);
+  vscode.commands.registerCommand("blogview.refreshEntry", () =>
+    provider.refresh()
+  );
+  vscode.workspace.onDidSaveTextDocument(() => provider.refresh());
+  vscode.workspace.onDidCreateFiles(() => provider.refresh());
+  vscode.workspace.onDidDeleteFiles(() => provider.refresh());
+  vscode.workspace.onDidRenameFiles(() => provider.refresh());
   return {
     extendMarkdownIt(md: MarkdownIt) {
       return md.use(markdownItHatena, getOptions());
---
+++ packages/blogview-vscode/src/treeView/provider.ts
@@ -0,0 +1,35 @@
+import vscode from "vscode";
+
+export class TreeDataProvider
+  implements vscode.TreeDataProvider<vscode.TreeItem>
+{
+  private _onDidChangeTreeData =
+    new vscode.EventEmitter<vscode.TreeItem | void>();
+  readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
+
+  refresh() {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: vscode.TreeItem) {
+    return element;
+  }
+
+  async getChildren(element?: vscode.TreeItem) {
+    const rootPath =
+      vscode.workspace.workspaceFolders &&
+      vscode.workspace.workspaceFolders.length > 0
+        ? vscode.workspace.workspaceFolders[0].uri
+        : undefined;
+    if (rootPath == undefined) {
+      vscode.window.showInformationMessage(
+        "ワークスペースを読み込めませんでした"
+      );
+      return Promise.resolve([]);
+    }
+    const files = await vscode.workspace.fs.readDirectory(
+      vscode.Uri.joinPath(rootPath, "entry")
+    );
+    return files.map(([file]) => new vscode.TreeItem(file));
+  }
+}
